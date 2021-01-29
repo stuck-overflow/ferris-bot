35825e6fd395f6a6db7b1dc7029f79dc6f649e19
ed-fish <edward.fish@surrey.ac.uk>
2021-01-29 16:27:00 +0000

    add temp file code format option

--- chat_code.rs
+++ chat_code.rs
@@ -1,9 +1,8 @@
-fn format_code(message: &str) {
-    println!("{}", message);
-    let path = "scrap.rs";
+fn save_code_format(message: &str) {
+    let path = "chat_code.rs";
     let _ = File::create(path);
     fs::write(path, message).expect("Unable to write");
     let mut tidy = Command::new("rustfmt");
-    tidy.arg("scrap.rs");
+    tidy.arg(path);
     tidy.status().expect("not working");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,55 @@
-
 use std::fs;
-use std::str;
 use std::fs::File;
 use std::io::prelude::*;
-use std::process::{Command, Stdio};use twitch_irc::login::StaticLoginCredentials;
+use std::io::{BufRead, BufReader, Error, Read, Seek, SeekFrom, Write};
+use std::process::{Command, Stdio};
+use std::str;
+use tempfile::tempdir;
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::message::{IRCMessage, PrivmsgMessage, ServerMessage};
 use twitch_irc::ClientConfig;
 use twitch_irc::TCPTransport;
 use twitch_irc::TwitchIRCClient;
-use twitch_irc::message::{IRCMessage,PrivmsgMessage, ServerMessage};
 
 fn parse_command(msg: PrivmsgMessage) {
     let first_word = msg.message_text.split_whitespace().next();
     let content = msg.message_text.replace(first_word.as_deref().unwrap(), "");
     match first_word {
         Some("!join") => println!("{}: Join requested", msg.sender.login),
-        Some("!Pythonsucks")=> println!("{}: This must be Lord", msg.sender.login),
-        Some("!Stonk")=> println!("{}: yOu shOULd Buy AMC sTOnKS", msg.sender.login),
-        Some("!C++")=> println!("{}: segmentation fault", msg.sender.login),
-        Some("!Dave")=> println!("{}", include_str!("../assets/dave.txt")),
-        Some("!Bazylia") => println!("{}", include_str!("../assets/bazylia.txt")),    
+        Some("!Pythonsucks") => println!("{}: This must be Lord", msg.sender.login),
+        Some("!Stonk") => println!("{}: yOu shOULd Buy AMC sTOnKS", msg.sender.login),
+        Some("!C++") => println!("{}: segmentation fault", msg.sender.login),
+        Some("!Dave") => println!("{}", include_str!("../assets/dave.txt")),
+        Some("!Bazylia") => println!("{}", include_str!("../assets/bazylia.txt")),
         Some("!Zoya") => println!("{}", include_str!("../assets/zoya.txt")),
         Some("!Discord") => println!("https://discord.gg/UyrsFX7N"),
-        Some("!code") =>  format_code(&content),
-        _ => {},
+        Some("!code") => save_code_format(&content),
+        _ => {}
     }
 }
 
+fn temp_code_format(message: &str) {
+    println!("{}", message);
+    let dir = tempdir().unwrap();
+    let mut file_path = dir.path().join("chat_code.rs");
+    let path = File::create(&file_path);
+    println!("{}", file_path.display());
+    let _ = fs::write(&file_path, message).expect("Unable to write");
+    let mut tidy = Command::new("rustfmt");
+    tidy.arg(&file_path);
+    tidy.status().expect("not working");
+    let content = fs::read_to_string(&file_path).expect("error reading file");
+    println!("{}", &content);
+}
 
-// Takes code after !code and writes into scrap.rs then formats with fmt - requires rustfmt installed (apt-get)
-fn format_code(message:&str) {
-    let path = "scrap.rs";
+fn save_code_format(message: &str) {
+    let path = "chat_code.rs";
     let _ = File::create(path);
     fs::write(path, message).expect("Unable to write");
     let mut tidy = Command::new("rustfmt");
-    tidy.arg("scrap.rs");
+    tidy.arg(path);
     tidy.status().expect("not working");
-  
 }
-
 
 #[tokio::main]
 pub async fn main() {
