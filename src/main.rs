871a92fea8a629d8fbb1c63ae949720ff4707298
ed-fish <edward.fish@surrey.ac.uk>
2021-02-04 17:46:56 +0000

    Added serenity Discord command code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 extern crate serenity;
+use serenity::http::Http;
+use serenity::model::id::ChannelId;
 use std::fs;
 use std::fs::File;
+use std::io::Read;
 use std::process::Command;
 use std::str;
+use std::sync::Arc;
 use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::message::{PrivmsgMessage, ServerMessage};
 use twitch_irc::ClientConfig;
 use twitch_irc::TCPTransport;
 use twitch_irc::TwitchIRCClient;
-mod discord_events;
+mod discord_commands;
 
-fn parse_command(msg: PrivmsgMessage) {
+async fn parse_command(msg: PrivmsgMessage, http: &Arc<Http>) {
     let first_word = msg.message_text.split_whitespace().next();
     let content = msg.message_text.replace(first_word.as_deref().unwrap(), "");
     let first_word = first_word.unwrap().to_lowercase();
@@ -25,9 +29,16 @@ fn parse_command(msg: PrivmsgMessage) {
         Some("!bazylia") => println!("{}", include_str!("../assets/bazylia.txt")),
         Some("!zoya") => println!("{}", include_str!("../assets/zoya.txt")),
         Some("!discord") => println!("https://discord.gg/UyrsFX7N"),
+        Some("!nothing") => nothing(&http).await,
         Some("!code") => save_code_format(&content),
         _ => {}
     }
+}
+
+async fn nothing(http: &Arc<Http>) {
+    println!("nothing received");
+    let id: u64 = 805839708198404106;
+    let _ = ChannelId(id).say(http, "DOES THIS WORK?").await;
 }
 
 fn save_code_format(message: &str) {
@@ -41,18 +52,26 @@ fn save_code_format(message: &str) {
 
 #[tokio::main]
 pub async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let mut file = File::open(".token").expect("Error loading Discord token");
+    let mut token = String::new();
+    file.read_to_string(&mut token)
+        .expect("Token file not found");
+
+    let http = Arc::new(Http::new_with_token(&token));
+
     // default configuration is to join chat as anonymous.
-    discord_events::activate_discord_bot();
     let config = ClientConfig::default();
     let (mut incoming_messages, client) =
         TwitchIRCClient::<TCPTransport, StaticLoginCredentials>::new(config);
 
+    let http2 = Arc::clone(&http);
     // first thing you should do: start consuming incoming messages,
     // otherwise they will back up.
     let join_handle = tokio::spawn(async move {
         while let Some(message) = incoming_messages.recv().await {
             match message {
-                ServerMessage::Privmsg(msg) => parse_command(msg),
+                ServerMessage::Privmsg(msg) => parse_command(msg, &http2).await,
                 _ => continue,
             }
         }
@@ -60,6 +79,8 @@ pub async fn main() {
 
     // join a channel
     client.join("stuck_overflow".to_owned());
+
+    discord_commands::init_discord_bot(Arc::clone(&http), &token).await;
     // keep the tokio executor alive.
     // If you return instead of waiting the background task will exit.
     join_handle.await.unwrap();
